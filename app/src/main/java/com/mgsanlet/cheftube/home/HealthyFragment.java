ac04aad3cf3c73d47b144ae95a9b661dd8280308
mgsanlet.dev <mgsanlet.dev@gmail.com>
2025-02-08 14:30:24 +0100

    healthy fr, fixed

--- app/src/main/java/com/mgsanlet/cheftube/home/HealthyFragment.java
+++ app/src/main/java/com/mgsanlet/cheftube/home/HealthyFragment.java
@@ -1,5 +1,6 @@
 package com.mgsanlet.cheftube.home;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -11,35 +12,26 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.bumptech.glide.Glide;
 import com.google.gson.Gson;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 import com.mgsanlet.cheftube.R;
 
-import org.json.JSONObject;
-
 public class HealthyFragment extends Fragment { //TODO DOC
     // -Declaring constants-
     private static final String BASE_URL = "https://world.openfoodfacts.org/api/v3/product/";
-    private static final String BASE_URL_IMG = "https://images.openfoodfacts.org/images/products/";
-
     // -Declaring UI elements-
     private ImageButton scanButton;
     private TextView productNameTView;
     private TextView nutriscoreTView;
     private TextView ecoscoreTView;
-    private ImageView productImageView;
 
     // -Declaring string resources-
     private String scanPromptStr;
@@ -57,7 +49,7 @@ public class HealthyFragment extends Fragment { //TODO DOC
         productNameTView = view.findViewById(R.id.productNameTView);
         nutriscoreTView = view.findViewById(R.id.nutriscoreTView);
         ecoscoreTView = view.findViewById(R.id.ecoscoreTView);
-        //productImageView = view.fi
+
 
 
         // -Initializing string resources-
@@ -85,7 +77,7 @@ public class HealthyFragment extends Fragment { //TODO DOC
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) { //TODO DOC
         super.onActivityResult(requestCode, resultCode, data);
-        //String barcode = "3017620422003"; example
+        String barcode = "3017620422003"; // example
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
         if (result != null) {
             if (result.getContents() != null) {
@@ -107,33 +99,23 @@ public class HealthyFragment extends Fragment { //TODO DOC
                 Request.Method.GET,
                 url,
                 null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        // Usar Gson para procesar la respuesta
-                        Log.d("API_RESPONSE", response.toString());
-                        processResponse(response.toString());
-                    }
+                response -> {
+                    // Usar Gson para procesar la respuesta
+                    Log.d("API_RESPONSE", response.toString());
+                    processResponse(response.toString());
                 },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        if (error.networkResponse != null) {
-                            int statusCode = error.networkResponse.statusCode;
-                            Toast.makeText(getContext(), "API: Error " + statusCode, Toast.LENGTH_LONG).show();
-                        }
+                error -> {
+                    if (error.networkResponse != null) {
+                        int statusCode = error.networkResponse.statusCode;
+                        Toast.makeText(getContext(), "API: Error " + statusCode, Toast.LENGTH_LONG).show();
                     }
                 });
 
         // Añadir la solicitud a la cola
         requestQueue.add(jsonObjectRequest);
-
-        String imageUrl = getProductImageUrl(barcode);
-        if (imageUrl != null) {
-            Glide.with(getContext()).load(imageUrl).into(productImageView);
-        }
     }
 
+    @SuppressLint("SetTextI18n")
     private void processResponse(String jsonResponse) { //TODO DOC
         // Usar Gson para mapear el JSON a una clase
         Gson gson = new Gson();
@@ -152,30 +134,12 @@ public class HealthyFragment extends Fragment { //TODO DOC
         }
     }
 
-    private String getProductImageUrl(String barcode) {
-        if (barcode.length() < 8) {
-            return null; // Evita errores si el código de barras es demasiado corto
-        }
-
-        String formattedBarcode = String.format("%s/%s/%s/%s",
-                barcode.substring(0, 3),
-                barcode.substring(3, 6),
-                barcode.substring(6, 9),
-                barcode.substring(9));
-
-        return BASE_URL_IMG + formattedBarcode + "/front_es.126.400.jpg";
-    }
-
     // Clases para mapear la respuesta JSON
     public static class ProductResponse { //TODO DOC
         private Product product;
 
         public Product getProduct() {
             return product;
-        }
-
-        public void setProduct(Product product) {
-            this.product = product;
         }
     }
 
